731359542e82db99e34fd8d61492e033e2cf659d
gp <feitian_014@163.com>
2016-09-27 16:26:03 +0800

    Serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.configure do
 
-Rails.application.configure do
+# Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
